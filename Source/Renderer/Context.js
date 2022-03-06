7015cee341946be2e77243e1201d0a359b9a017c
TJKoury <TJKoury@gmail.com>
2022-03-06 15:36:49 -0500

    WebGL 1 compatibility
    `gl.MAX_SAMPLES` does not exist in WebGL 1, do a quick check and if not present, set to WebGLConstants.MAX_SAMPLES.

--- Source/Renderer/Context.js
+++ Source/Renderer/Context.js
@@ -256,7 +256,7 @@ function Context(canvas, options) {
     gl.MAX_VERTEX_UNIFORM_VECTORS
   ); // min: 128
 
-  ContextLimits._maximumSamples = gl.getParameter(gl.MAX_SAMPLES);
+  ContextLimits._maximumSamples = gl.MAX_SAMPLES ? gl.getParameter(gl.MAX_SAMPLES) : WebGLConstants.MAX_SAMPLES;
 
   const aliasedLineWidthRange = gl.getParameter(gl.ALIASED_LINE_WIDTH_RANGE); // must include 1
   ContextLimits._minimumAliasedLineWidth = aliasedLineWidthRange[0];
